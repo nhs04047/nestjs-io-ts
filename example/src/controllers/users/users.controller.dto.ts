39047de7ef1ce484bf1141aad5a7be690904f7d5
BoHyeon <nhs04047@gmail.com>
2025-01-15 00:18:35 +0900

    example: Email 예시 추가가

--- example/src/controllers/users/users.controller.dto.ts
+++ example/src/controllers/users/users.controller.dto.ts
@@ -1,10 +1,10 @@
 import * as t from 'io-ts';
-import { createIoTsDto, IoTsTypes } from 'nestjs-io-ts';
+import { createIoTsDto, Email } from 'nestjs-io-ts';
 
 const CreateUserDtoC = t.type({
   id: t.string,
   name: t.string,
-  email: t.string,
+  email: Email,
   age: t.union([t.number, t.undefined]),
 });
 
